3d2d4f614016a3f29e16a3faae377ab90fe8e3f9
kuche <guschingb@yandexlyceum.ru>
2021-12-17 21:27:08 +0300

    new usage. for nine grades. now we store data separately and use var to from file name/ test on heroku

--- main.py
+++ main.py
@@ -9,11 +9,10 @@ students_grades = []
 for student in students:
     temp = student.split()
     if temp[0] not in students_dict:
-        students_dict[temp[0]] = [temp[1:]]
+        students_dict[temp[0]] = [temp[1]]
         students_grades.append(temp[0])
     else:
         students_dict[temp[0]].append(temp[1])
-
 app = Flask(__name__)
 
 
@@ -29,12 +28,17 @@ def index():
               href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
               integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
               crossorigin="anonymous">
+                        <style>
+                               form {
+                                width: 500px; 
+                               }
+                              </style>
                         <title>Распределение</title>
                       </head>
                       <body>
                         <form method="post">
-                            <select name="grade" class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
-                              <option selected>Выберите класс</option>
+                            <h1>Выберите класс</h1> 
+                            <select name="grade" class="form-select">
                               <option value="1">9а</option>
                               <option value="2">9б</option>
                               <option value="3">9в</option>
@@ -51,79 +55,61 @@ def index():
         grade = students_grades[int(request.form['grade']) -1]
         print(surname, grade)
         if surname in students_dict[grade]:
-            variant = students_dict[grade].index(surname) % 2 + 1
+            variant = students_dict[grade].index(surname) % 3 + 1
             return redirect(f'result/{variant}')
         else:
             return '''<!doctype html>
-                                <html lang="en">
-                                  <head>
-                                    <meta charset="utf-8">
-                                    <link rel="stylesheet"
-                          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-                          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-                          crossorigin="anonymous">
-                                    <title>Распределение</title>
-                                  </head>
-                                  <body>
-                                    <h1>Ошибка ввода: вводите только фамилию и только в именительном падеже</h1>
-                                    <form method="post">
-                                        <select name="grade" class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
-                                          <option selected>Выберите класс</option>
-                                          <option value="1">9а</option>
-                                          <option value="2">9б</option>
-                                          <option value="3">9в</option>
-                                        </select>             
-                                        <h1>Введите фамилию</h1>                                    
-                                        <input name="surname">
-                                        <button type="submit" class="btn btn-primary btn-sm">Отправить</button>
-                                    </form>
-                                  </body>
-                                </html>'''
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <link rel="stylesheet"
+              href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+              integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+              crossorigin="anonymous">
+                        <style>
+                               form {
+                                width: 500px; 
+                               }
+                              </style>
+                        <title>Распределение</title>
+                      </head>
+                      <body>
+                        <form method="post">
+                            <h1>Ошибка ввода: проверьте класс и фамилию!</h1>
+                            <h1>Выберите класс</h1> 
+                            <select name="grade" class="form-select">
+                              <option value="1">9а</option>
+                              <option value="2">9б</option>
+                              <option value="3">9в</option>
+                            </select>                  
+                            <h1>Введите фамилию</h1>                        
+                            <input name="surname">
+                            <button type="submit" class="btn btn-primary btn-sm">Отправить</button>
+                        </form>
+                      </body>
+                    </html>'''
+
 @app.route('/result/<variant>')
 def result(variant):
-    if variant == '1':
-        return '''<!doctype html>
-                        <html lang="en">
-                          <head>
-                            <meta charset="utf-8">
-                            <title>Распределение</title>
-                          </head>
-                          <body>
-                            <h1>111111Ваша ссылка на тест</h1>
-                            <a href='https://forms.gle/qmkVmLMKGkcx6d4v7'> начать </a>
-                          </body>
-                        </html>'''
-    elif variant == '2':
-        return '''<!doctype html>
-                                <html lang="en">
-                                  <head>
-                                    <meta charset="utf-8">
-                                    <title>Распределение</title>
-                                  </head>
-                                  <body>
-                                    <h1>22222Ваша ссылка на тест</h1>
-                                    <a href='https://forms.gle/etmZi7nsngPXektL8'> начать </a>
-                                  </body>
-                                </html>'''
-    else:
-        return '''<!doctype html>
-                                <html lang="en">
-                                  <head>
-                                    <meta charset="utf-8">
-                                    <title>Распределение</title>
-                                  </head>
-                                  <body>
-                                    <h1>Forbidden!</h1>
-                                  </body>
-                                </html>'''
+    return f'''<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <title>Распределение</title>
+                      </head>
+                      <body>
+                        <img src="/static/{variant}.jpg" alt='Что-то пошло не так'>
+                      </body>
+                    </html>'''
+
 
 
 
 # for local tests
-if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+# if __name__ == '__main__':
+#     app.run(port=8080, host='127.0.0.1')
 
 # for heroku
-# if __name__ == '__main__':
-#     port = int(os.environ.get("PORT", 5000))
-#     app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
