a52322eafd553c2e3ab9312879efd46b54439755
kuche <guschingb@yandexlyceum.ru>
2021-12-17 22:38:46 +0300

    heroku cant store images? ok replaced them to the texts

--- main.py
+++ main.py
@@ -154,10 +154,10 @@ def result(variant):
 
 
 # for local tests
-if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+# if __name__ == '__main__':
+#     app.run(port=8080, host='127.0.0.1')
 
 # for heroku
-# if __name__ == '__main__':
-#     port = int(os.environ.get("PORT", 5000))
-#     app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
